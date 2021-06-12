0e36b854239113eebf79259ef60e0212b614ad13
steffent1493 <steffent1493@gmail.com>
2021-06-11 22:20:59 -0400

    mission accomplished

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+const userUrl = "http://localhost:3000/users";
+
+let bodyData = {
+    name: "Steve",
+    email: "steve@steve.com"
+};
+
+let objStuff = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(bodyData)
+};
+
+function submitData () {
+ return fetch(userUrl, objStuff)
+    .then(response => response.json())
+    .then(getID)
+    .catch(errorFunc)
+}
+
+const bodyTag = document.querySelector(`body`)
+
+function getID(object) {
+    document.body.innerHTML = object.id
+}
+
+function errorFunc(error) {
+    document.body.innerHTML = error.message
+}
+    
